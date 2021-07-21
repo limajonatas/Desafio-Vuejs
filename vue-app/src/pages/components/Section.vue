2e56cc51737b252fd7e96c3df324a6f190729fff
wlfoj <wolivej@gmail.com>
2021-07-20 22:31:18 -0300

    added component DialogError

--- vue-app/src/pages/components/Section.vue
+++ vue-app/src/pages/components/Section.vue
@@ -1,18 +1,22 @@
 <template>
     <section>
         <BarChart :response="{aaa:123}"/>//
-        <DialogLoading/>
+        <DialogLoading />
+        <DialogError />
     </section>
 </template>
 
 <script>
     import BarChart from './BarChart'
     import DialogLoading from './Loading'
+    import DialogError from './Error'
+
     export default {
     name: 'Section',
     components: {
         BarChart,
-        DialogLoading
+        DialogLoading,
+        DialogError
     },
     mounted(){
         // Fazer a requisição para API

bdf685d5020751934c599b52e57aa344ce9ed0aa
Washington Oliveira <wolivej@gmail.com>
2021-07-22 23:07:38 -0300

    correct names

--- vue-app/src/pages/components/BarChart.vue
+++ vue-app/src/pages/components/BarChart.vue
@@ -7,7 +7,7 @@
 
 	Vue.use(VueFusionCharts, FusionCharts, Column2D, FusionTheme);
 
-	//const chartData = [];
+	// Dados para o gráfico com os 7 países com mais casos
 	const dataSourceBig = {
 		chart: {
 			caption: "7 Países com mais casos",
@@ -17,8 +17,10 @@
 			numbersuffix: "",
 			theme: "fusion"
 		},
-		data: []//chartData
+		data: []
 	};
+
+	// Dados para o gráfico com todos os países 
 	const dataSourceTotal = {
 		chart: {
 			caption: "Casos de COVID-19",
@@ -28,7 +30,7 @@
 			numbersuffix: "",
 			theme: "fusion"
 		},
-		data: []//chartData
+		data: []
 	};
 
 	export default {
@@ -47,36 +49,45 @@
 		}, 
 		/**/
 		props: {
-			response: Array// ou Array, acho que é Array ; Object
+			response: Array
 		},
 		/**/
 		methods: {
-			transformDataTotal: function (total) {
-				var pais;
-				var qtd;
-				for (var i=0; i<total.length; i++){
-					pais = total[i].Country
-					qtd = total[i].TotalConfirmed
-					//total.push({label:pais, value:qtd})
-					this.dataSourceTotal.data.push({label:pais, value:qtd})
+			// Este método, insere os dados no formato requerido pelo fusion chart
+			transformDataTotal: function (data, vetor) {
+				
+				var country; // nome do país
+				var qtd;	 // quantidade de casos
+				for (var i=0; i < data.length; i++){
+					country = data[i].Country
+					qtd = data[i].TotalConfirmed
+					vetor.push({label:country, value:qtd})
 				}
 			},
-			get7Big: function (maiores){// Mudar, fazer sem o deletar
-				var indexm = 0
+			// Este método pega os países com mais casos confirmados
+			getBiggerCases: function (dataAllCountries){
+				// Obtendo uma cópia da lista para evitar danos futuros
+				var copyData = dataAllCountries.slice(0, dataAllCountries.length);
+				// Inicializando variáveis para controle
+				var lisInter = []
+				var index = 0
+				var maior = 0
+				// Repete 7 vezes
 				for (var i = 0; i < 7; i++){
-					var maior = {label:'', value:0}
-					for (var j = 0; j < maiores.length; j++){
-						if (maiores[j].TotalConfirmed > maior.value){
-							maior.label = maiores[j].Country
-							maior.value = maiores[j].TotalConfirmed
-							indexm = j
+					index = 0
+					maior = 0
+					// Percorre lista inteira
+					for (var j = 0; j < copyData.length; j++){
+						// Se o valor dos casos for maior, então será o novo maior
+						if (copyData[j].TotalConfirmed > maior){
+							maior = copyData[j].TotalConfirmed
+							index = j // para não repetir o mesmo elemento data7Countries
 						}
 					}
-					maiores.splice(indexm, 1);
-					
-					this.dataSourceBig.data.push(maior)
+					lisInter.push(copyData[index])
+					copyData.splice(index, 1);// Para não passar pelo mesmo elemento novamente
 				}
-				
+				this.transformDataTotal(lisInter,this.dataSourceBig.data) // Adequa o dado para o formato requerido
 			}
 		},
 		/**/
@@ -84,9 +95,8 @@
 			// criar aqui o metodo para transformar os dados 
 			//this.dataSourceTotal.data = this.response
 			// Criar metodos
-			this.transformDataTotal(this.response)
-			this.get7Big(this.response)
-			console.log(this.dataSourceTotal.data)
+			this.transformDataTotal(this.response, this.dataSourceTotal.data)// transforma os dados no formato pedido pelo chart
+			this.getBiggerCases(this.response)// cria uma lista com os 7 maiores
 		}
 	}
 
@@ -94,6 +104,7 @@
 
 <template>
 	<div class="chart-container" >
+		<!-- Gráfico para todos os países-->
 		<div class = "card-chart">
 			<fusioncharts class='teste'
 			:type="type"
@@ -103,6 +114,7 @@
 			:dataSource="dataSourceTotal"
 			/>
 		</div>
+		<!-- Gráfico para os sete países com mais casos-->
 		<div class = "card-chart">
 			<fusioncharts
 			:type="type"
@@ -122,10 +134,12 @@
 		width:100%;
 		align-items: center
 	}
+	.chart-container:last-child{
+		margin-bottom: 32px;
+	}
 	.card-chart{
 		margin-top: 32px;
 		width:80%;
-		
 	}
 
 </style>
